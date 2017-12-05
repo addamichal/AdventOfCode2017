a681ffe87b253a4bbdda15a7931830d0731ccc77
addamichal <addamichal@gmail.com>
2017-12-05 10:26:17 +0100

    adding day 4

---
+++ Day5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            var answer1 = GetAnswer(lines, (i) => i + 1);
+            var answer2 = GetAnswer(lines, (i) => i >= 3 ? i - 1 : i + 1);
+            
+            Console.WriteLine(answer1);
+            Console.WriteLine(answer2);
+        }
+
+        private static int GetAnswer(string[] lines, Func<int, int> GetValue)
+        {
+            int currentPosition = 0;
+            var array = lines.Select(w => int.Parse(w)).ToArray();
+            var arrayLenght = array.Length;
+
+            int steps = 0;
+            while (true)
+            {
+                int nextStep = array[currentPosition];
+
+                array[currentPosition] = GetValue(array[currentPosition]);               
+                currentPosition += nextStep;
+                steps++;
+
+                if (currentPosition >= arrayLenght)
+                {
+                    break;
+                }
+            }
+            return steps;
+        }
+    }
+}
